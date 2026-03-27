4df22bee59e2b7fd5d9fc910d266dfb1ee9468a8
vlazed <35244845+vlazed@users.noreply.github.com>
2026-03-27 17:37:04 -0400

    Fix decimate not evaluating in [0,1)

--- lua/autorun/sh_softlamp_controller.lua
+++ lua/autorun/sh_softlamp_controller.lua
@@ -315,14 +315,15 @@ if CLIENT then
 		for k, lamp in pairs(SoftLampsBounceTable) do
 			for Pix, PixTable in pairs(lamp) do
 				count = count + 1
-				if math.random(0, 1) < ratio then
+				local roll = math.random()
+				if roll < ratio then
 					removeCount = removeCount + 1
 					lamp[Pix] = nil
 				end
 			end
 		end
 
-		MsgN("Removed ", removeCount, "bounces out of ", count)
+		MsgN("Removed ", removeCount, " bounces out of ", count)
 	end)
 
 	concommand.Add("lightbounce_save", function(ply, cmd, args, argStr)
